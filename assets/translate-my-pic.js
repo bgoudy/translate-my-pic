e560ed9706942a891fd4fece3ce95892e87a2d02
Kayley Bannister <kayley.bannister@gmail.com>
2019-07-20 11:35:21 -0400

    added js and html to preview image after it is uploaded and created containers for the html to push everything in

--- assets/translate-my-pic.js
+++ assets/translate-my-pic.js
@@ -190,5 +190,22 @@ $(document).ready(function()
       }
     }
    }
+
+   //testing image upload
+   function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageUploaded').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    };
+    };
+
+      $("#fileToUpload").change(function(){
+    readURL(this);
+      });
   });
    
