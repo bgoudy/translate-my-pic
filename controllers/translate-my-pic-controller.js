cf6f7dbec6e93a4d52f09e2c17c9cf2fe7712971
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-09-06 00:52:32 -0400

    Set up initial application platform, folder structure, and server.js

---
+++ controllers/translate-my-pic-controller.js
@@ -0,0 +1,9 @@
+// Required dependencies and packages
+var express = require("express");
+var router = express.Router();
+var db = require("../models");
+
+
+
+// Exports the translate-my-pic-plus API routes
+module.exports = router;
